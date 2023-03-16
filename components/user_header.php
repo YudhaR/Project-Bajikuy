733bd6565b5ac6f86c2e25eb92cafbd5db0d40a1
Yudha Rizqia <Yudharizqia182@gmail.com>
2023-03-16 16:57:29 +0700

    beranda

---
+++ components/user_header.php
@@ -0,0 +1,50 @@
+<!--==================== HEADER ====================-->
+<header class="header" id="header">
+            <nav class="nav container">
+                <a href="#" class="nav__logo">
+                    <img src="./img/bajikuyyy.png" alt="logo">
+                </a>
+
+                <div class="nav__menu" id="nav-menu">
+                    <ul class="nav__list">
+                        <li class="nav__item">
+                            <a href="#beranda" class="nav__link active-link">Beranda</a>
+                        </li>
+
+                        <li class="nav__item">
+                            <a href="#tentang"about class="nav__link">Tentang</a>
+                        </li>
+
+                        <li class="nav__item">
+                            <a href="#cerita" class="nav__link">Cerita</a>
+                        </li>
+
+                        <li class="nav__item">
+                            <a href="#favorit" class="nav__link">Favorit</a>
+                        </li>
+
+                        <li class="nav__item">
+                            <a href="#terbaru" class="nav__link">Terbaru</a>
+                        </li>
+                    </ul>
+
+                    <!-- Close button -->
+                    <div class="nav__close" id="nav-close">
+                        <i class="ri-close-line"></i>
+                    </div>
+
+                </div>
+
+                <div class="nav__buttons">
+                    <!-- Theme change button -->
+                    <i class="ri-moon-line change-theme" id="theme-button"></i>
+
+                    <!-- Toggle button -->
+                    <div class="nav__toggle" id="nav-toggle">
+                        <i class="ri-apps-2-fill"></i>
+                    </div>
+                </div>
+                 
+            </nav>
+            
+        </header>
---
+++ home.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <!--=============== FAVICON ===============-->
+        <link rel="shortcut icon" href="./img/icon.png" type="image/x-icon">
+
+        <!--=============== REMIXICONS ===============-->
+        <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
+
+        <!--=============== CSS ===============-->
+        <link rel="stylesheet" href="./css/home.css">
+
+        <title>Bajikuy</title>
+    </head>
+    <body>
+    
+
+        <?php include 'components/user_header.php'; ?>
+
+        <!--==================== MAIN ====================-->
+        <section class="home section" id="home">
+            <div class="home_container container grid">
+                <div class="home_data">
+                    <span class="home_subtitle">Temukan secangkir gelasmu hanya di</span>
+                    <h4 class="home_title">BAJIKUYYY STORE</h4>
+                    <div class="home_list">
+                        <h4 class="dropdown">KUNJUNGI</h4>
+                        <ul class="home_items">
+                            <li class="home_item"><a href="https://www.facebook.com/watch/Masterchef/"target="_blank" class="home_link">Facebook</a></li>
+                            <li class="home_item"><a href="https://www.instagram.com/ydhrizqi._/"target="_blank" class="home_link">Instagram</a></li>
+                            <li class="home_item"><a href="https://twitter.com/ydhrizqi_"target="_blank" class="home_link">Twitter</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="home_description">
+                    <p class="desc">Bajikuyyy adalah platform yang bertujuan melestarikan minuman hangat tradisional Indonesia dengan merekomendasikan minuman dan tempat terdekat untuk mendapatkannya, yang memiliki banyak khasiat dan nilai manfaat yang terkandung dalam setiap gelas atau cangkir minuman tersebut.</p>
+                    <div class="box">
+                        <a href="">
+                            <div class="boxp">
+                                <h2 class="box_title">Cerita</h2>
+                                <img src="./img/1.png" alt="">
+                            </div>
+                        </a>
+                        <a href="">
+                            <div class="boxp">
+                                <h2 class="box_title">Khasiat</h2>
+                                <img src="./img/2.png" alt="">
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+
+
+
+        <!--========== SCROLL UP ==========-->
+        <a href="#" class="scrollup" id="scroll-up">
+            <i class="ri-arrow-up-line"></i>
+        </a>
+
+        <!--=============== SCROLLREVEAL ===============-->
+        <script src="./js/scrollreveal.min.js"></script>
+
+        <!--=============== Header JS ===============-->
+        <script src="./js/header.js"></script>
+    </body>
